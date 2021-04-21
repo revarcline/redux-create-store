dd278fd06148da876f57bfa9c91e1f816afe4822
Alex Cline <rev.a.r.cline@gmail.com>
2021-04-21 01:31:50 -0500

    Done.

--- js/createStore.js
+++ js/createStore.js
@@ -1,28 +1,34 @@
-let state;
+const createStore = () => {
+  let state;
 
-function reducer(state = { count: 0 }, action) {
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    render();
+  };
+
+  const getState = () => state;
+
+  return { dispatch, getState };
+};
+
+const reducer = (state = { count: 0 }, action) => {
   switch (action.type) {
-    case 'INCREASE_COUNT':
+    case "INCREASE_COUNT":
       return { count: state.count + 1 };
 
     default:
       return state;
   }
 };
-
-function dispatch(action){
-  state = reducer(state, action);
-  render();
+const render = () => {
+  let container = document.getElementById("container");
+  container.textContent = store.getState().count;
 };
 
-function render() {
-  let container = document.getElementById('container');
-  container.textContent = state.count;
-};
+let store = createStore();
+store.dispatch({ type: "@@INIT" });
 
-dispatch({ type: '@@INIT' })
-let button = document.getElementById('button');
-
-button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
-})
+let button = document.getElementById("button");
+button.addEventListener("click", () => {
+  store.dispatch({ type: "INCREASE_COUNT" });
+});
